e58e164f230337b1d52ea5ca9283965113a1fd50
huy923 <huyxam233@gmail.com>
2024-10-12 14:18:15 +0700

    new update

--- do_an_nhom_15/Areas/admin/Controllers/HomeController.cs
+++ do_an_nhom_15/Areas/admin/Controllers/HomeController.cs
@@ -10,9 +10,18 @@ namespace do_an_nhom_15.Areas.admin.Controllers
             return View();
         }
         public IActionResult Button()
-        { 
-            return View(); 
+        {
+            return View();
         }
-        
+        public IActionResult Typography(){return View();}
+        public IActionResult Widget() { return View();}
+        public IActionResult Table() { return View(); }
+        public IActionResult Signup() { return View(); }
+        public IActionResult Signin() { return View(); }
+        public IActionResult Form() { return View(); }
+        public IActionResult Element() { return View(); }
+        public IActionResult Chart() { return View(); }
+        public IActionResult Blank() { return View(); }
+        public IActionResult _404() { return View(); }
     }
 }
