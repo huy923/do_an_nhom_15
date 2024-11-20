9d94df1884ecb470958cf3f9b2e6c7ea075edefa
havi-949604 <havi7526@gmail.com>
2024-11-20 23:02:43 +0700

    havi đang làm register

---
+++ do_an_nhom_15/Areas/Models/havi.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace do_an_nhom_15.Areas.admin.Models
+{
+    [Table("AdminUsers")]
+    public class havi
+    {
+        [Key]
+        public int AdminId { get; set; }
+        public string? Username { get; set; }
+        public string? FullName { get; set; }
+        public string? Password { get; set; }
+        public bool IsActive { get; set; }
+
+
+    }
+}
