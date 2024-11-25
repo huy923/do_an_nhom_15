6756f029dead1352b75adef9edf44b03087e595b
havi-949604 <havi7526@gmail.com>
2024-11-26 02:16:15 +0700

    admin controller with entity fremwork

---
+++ do_an_nhom_15/Areas/admin/Controllers/AdminUsersController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using do_an_nhom_15.Models;
+
+namespace do_an_nhom_15.Areas.admin.Controllers
+{
+    [Area("admin")]
+    public class AdminUsersController : Controller
+    {
+        private readonly CoffeeShopDbContext _context;
+
+        public AdminUsersController(CoffeeShopDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: admin/AdminUsers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.AdminUsers.ToListAsync());
+        }
+
+        // GET: admin/AdminUsers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var adminUser = await _context.AdminUsers
+                .FirstOrDefaultAsync(m => m.AdminId == id);
+            if (adminUser == null)
+            {
+                return NotFound();
+            }
+
+            return View(adminUser);
+        }
+
+        // GET: admin/AdminUsers/Create
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        // POST: admin/AdminUsers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Add([Bind("AdminId,Username,Password,FullName,IsActive,CreatedAt")] AdminUser adminUser)
+        {
+            adminUser.IsActive = true;
+            if (ModelState.IsValid)
+            {
+                _context.Add(adminUser);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(adminUser);
+        }
+
+        // GET: admin/AdminUsers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var adminUser = await _context.AdminUsers.FindAsync(id);
+            if (adminUser == null)
+            {
+                return NotFound();
+            }
+            return View(adminUser);
+        }
+
+        // POST: admin/AdminUsers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AdminId,Username,Password,FullName,IsActive,CreatedAt")] AdminUser adminUser)
+        {
+            if (id != adminUser.AdminId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(adminUser);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AdminUserExists(adminUser.AdminId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(adminUser);
+        }
+
+        // GET: admin/AdminUsers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var adminUser = await _context.AdminUsers
+                .FirstOrDefaultAsync(m => m.AdminId == id);
+            if (adminUser == null)
+            {
+                return NotFound();
+            }
+
+            return View(adminUser);
+        }
+
+        // POST: admin/AdminUsers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var adminUser = await _context.AdminUsers.FindAsync(id);
+            if (adminUser != null)
+            {
+                _context.AdminUsers.Remove(adminUser);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AdminUserExists(int id)
+        {
+            return _context.AdminUsers.Any(e => e.AdminId == id);
+        }
+    }
+}
